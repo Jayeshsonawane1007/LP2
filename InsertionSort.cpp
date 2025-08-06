f942d1270a601e49cc3ec63be9e8de2c0d64dacf
Jayesh Pratap Sonawane <149810387+Jayeshsonawane1007@users.noreply.github.com>
2025-08-06 11:08:31 +0530

    InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];      // Element to be inserted
+        int j = i - 1;
+
+        // Move elements that are greater than key to one position ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;  // Place key at correct position
+    }
+}
+
+void printArray(int arr[], int n) {
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insertionSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
